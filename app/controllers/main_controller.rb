8cba64ba037754e7ef9207c39f0f5f1ee3854606
DreadPirateNemo <x4nocide@gmail.com>
2023-10-02 20:47:33 -0500

    end of night v2

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
   def index
-    flash[:notice] = "Logged in successfully"
-    flash[:alert] = "Invalid credentials"
   end
 end
+
+# :begin
+#     flash.now[:notice] = "Logged in successfully"
+#     flash.now[:alert] = "Invalid credentials"
+# :end
